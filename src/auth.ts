d55b07b82108ecc8d0815cd70fcd6518da81ac99
小鹏 <xiaopeng@192.168.1.120>
2026-04-15 12:39:13 +0800

    fix: NextAuth v5加trustHost解决Configuration错误

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import Facebook from "next-auth/providers/facebook"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
